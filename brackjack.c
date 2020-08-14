46f787b5b33e7761bcc66b0d1a0949df1bab26a7
chisato-k <chisato-k@users.noreply.github.com>
2020-08-14 15:29:01 +0900

    opengl
    gultを使って画面表示を行った

--- brackjack.c
+++ brackjack.c
@@ -1,29 +1,52 @@
 #include<stdio.h>
-//#include<GL/glut.h>
+#include <GL/glut.h>
+#include <unistd.h>
 #include <stdlib.h>
 #include <time.h>
-// データ数
+#include <string.h>
 #define    N    52
-// 乱数の最小値
 #define    MIN    0
-// 乱数の最大値
 #define    MAX     52
 #define    heart 0
 #define    spade  1
 #define    diamond 2
 #define    club 3
+#define    betting 0
+#define    start  1
+#define    rule 2
+#define    end 3
+#define    gameover 4
+#define    win 0
+#define    lose 1
+#define    draw 2
+#define    brackjack 3
+#define    just 4
+#define    burst 5
+char s[4]={0};
+int tip = 5;
+int bet = 0;
+int mode = betting;
+int pmode;
+int x,y;
+int errar=1;
+int result1;
 typedef struct cards {
-    int num;
-    char mark;
+    char num[3];
+    int mark;
     int point;
 } cards;
+
 cards deck[N];
 cards dealer[12];
 cards player[12];
 int dcount = 0;
 int pcount = 0;
-
+int d_judge = 0,p_judge = 0;
 void shuffle(void){
+    if(mode==gameover){tip=5;}
+    dcount=0,pcount=0,d_judge=0,p_judge=0,mode=betting,bet=0,result1=10;
+    memset(dealer,0,sizeof(dealer));
+    memset(player,0,sizeof(player));
     int rnd;
     int mark;
     int bFind;
@@ -43,54 +66,74 @@ void shuffle(void){
             }
         }while(bFind);
         data[i] = rnd;
-        deck[i].num = rnd%13+1;
-        mark = rnd/13;
-        switch (mark) {
-            case heart:
-                deck[i].mark='H';
+        int x = rnd%13+1;
+        switch (x) {
+            case 10:
+                deck[i].num[0] = '1';deck[i].num[1] = '0';
+                deck[i].point = 10;
                 break;
-            case diamond:
-                deck[i].mark='D';
-                break;
-            case spade:
-                deck[i].mark='S';
-                break;
-            case club:
-                deck[i].mark='C';
-                break;
-            default:
-                break;
-        }
-        switch (deck[i].num) {
             case 11:
+                deck[i].num[0] = 'J';
+                deck[i].point = 10;
+                break;
             case 12:
+                deck[i].num[0] = 'Q';
+                deck[i].point = 10;
+                break;
             case 13:
+                deck[i].num[0] = 'K';
                 deck[i].point = 10;
                 break;
             case 1:
+                deck[i].num[0] = 'A';
                 deck[i].point = 11;
                 break;
             default:
-                deck[i].point = deck[i].num;
+                deck[i].num[0] = '0' + x;
+                deck[i].point = x;
                 break;
         }
+        mark = rnd/13;
+        switch (mark) {
+            case heart:
+                deck[i].mark=heart;
+                break;
+            case diamond:
+                deck[i].mark=diamond;
+                break;
+            case spade:
+                deck[i].mark=spade;
+                break;
+            case club:
+                deck[i].mark=club;
+                break;
+            default:
+                break;
+        }
+        
     }
 }
-
 void deal_card(void){
-    int i;
     dealer[0]=deck[0],dealer[1]=deck[1];
     player[0]=deck[2],player[1]=deck[3];
     dcount+=2;
     pcount+=2;
 }
-
 void get_card(cards* deck1,int n){
     static int count = 4;
     deck1[n]=deck[count];
     count++;
 }
+void output(double x, double y, char *string)
+{
+  int len, i;
 
+  glRasterPos2f(x, y);
+  len = (int) strlen(string);
+  for (i = 0; i < len; i++) {
+    glutBitmapCharacter(GLUT_BITMAP_TIMES_ROMAN_24, string[i]);
+  }
+}
 int judge_dealer(void){
     int sum=0,i;
     for(i=0;i<dcount;i++){
@@ -142,93 +185,405 @@ int judge_player(void){
     }
     return 0;
 }
-
-int input_screen(){
-    int num;
-    printf("1.HIT(カードを引く)  2.STAND(現在のカードで勝負)\n");
-    printf("Enter the number...");
-    scanf("%d",&num);
-    return num;
-}
-
-void game_screen(int hide){
-    int i=0,n=dcount;
-    printf("dealer     ");
-    if(hide==1){
-        n=2;
-        i=1;
-        printf("|  |");
-    }
-    for(i;i<n;i++){
-        printf("|%c%d|",dealer[i].mark,dealer[i].num);
-    }
-    printf("\n");
-    printf("player     ");
-    for(i=0;i<pcount;i++){
-        
-        printf("|%c%d|",player[i].mark,player[i].num);
-    }
-    printf("\n");
-}
-void judge_game(void){
+int judge_game(void){
     int dsum=0,psum=0;
     int i;
+    int result;
     for(i=0;i<dcount;i++){
         dsum += dealer[i].point;
     }
     for(i=0;i<pcount;i++){
         psum += player[i].point;
     }
-    game_screen(0);
     if(dsum!=21&&psum==21){
-        printf("black jack!\n");
-    }
-    else if(dsum>21||(dsum<21&&psum>dsum)){
-        printf("you win!\n");
-    }
-    else if(dsum==21||(dsum<21&&psum<dsum)){
-        printf("you lose!\n");
-    }
-    else{
-        printf("draw!\n");
-    }
-}
-int main()
-{
-    int d_judge = 0,p_judge = 0;
-    int n;
-    shuffle();
-    deal_card();
-    game_screen(1);
-    d_judge = judge_dealer();
-    p_judge = judge_player();
-    
-    while(p_judge==0){
-        n = input_screen();
-        if(n==1){
-            get_card(player,pcount);
-            pcount++;
-            game_screen(1);
-            p_judge = judge_player();
+        glColor3d(1.0, 0.0, 0.0);
+        if(pcount==2){
+            
+            result=brackjack;
+            tip+=(double)bet*2.5;
         }
         else{
-            p_judge=1;
+            
+            result=just;
+            tip+=(double)bet*2.2;
         }
     }
-    if(p_judge==2){
-        printf("burst!\n");
-        return 0;
+    else if(dsum>21||(dsum<21&&psum>dsum)){
+        tip+=bet*2;
+        result = win;
     }
-    while(d_judge==0){
-        get_card(dealer,dcount);
-        dcount++;
-        d_judge = judge_dealer();
+    else if(dsum==21||(dsum<21&&psum<dsum)){
+        result = lose;
     }
-    judge_game();
-    return 0;
+    else{
+        tip+=bet;
+        result = draw;
+    }
+    if(tip==0){
+        mode = gameover;
+    }
+    else{
+        mode = end;
+    }
+    glutPostRedisplay();
+    return result;
+}
+void draw_heart(float cx,float cy){
+    int i,n=100;
+    double pi=3.14159;
+    double rate;
+    float x,y,r=0.05900/2.0;
+    glColor3d(1.0, 0.0, 0.0);
+    glBegin(GL_POLYGON);
+    glVertex2d(cx,  cy-0.09);
+    glVertex2d(cx+2.0*r,  cy-0.01);
+    for(i=0;i<n;i++){
+          rate = (double)i/n;
+          x=r*cos(2.0*pi*rate)+cx;
+          y=r*sin(2.0*pi*rate)+cy;
+          glVertex2d(x+r,y);
+    }
+    glVertex2d(cx,  cy-r/2);
+    for(i=0;i<n;i++){
+        rate = (double)i/n;
+        x=r*cos(2.0*pi*rate)+cx;
+        y=r*sin(2.0*pi*rate)+cy;
+        glVertex2d(x-r,y);
+    }
+    glEnd();
+}
+void draw_diamond(float x,float y){
+    glColor3d(1.0, 0.0, 0.0);
+    glBegin(GL_POLYGON);
+    glVertex2d(x-0.04,  y);
+    glVertex2d(x,  y-0.07);
+    glVertex2d(x+0.04,  y);
+    glVertex2d(x,  y+0.07);
+    glEnd();
+}
+void draw_club(double cx,double cy){
+    int i,n=100;
+    double pi=3.14159;
+    double rate;
+    float x,y,r=0.05800/2.0;
+    
+    glColor3d(0.0, 0.0, 0.0);
+    glBegin(GL_POLYGON);
+    glVertex2d(cx, cy);
+    glVertex2d(cx-0.01, cy-0.09);
+    glVertex2d(cx+0.01, cy-0.09);
+    glVertex2d(cx, cy);
+    for(i=-n-5;i<=0;i++){
+        rate = (double)i/n;
+        x=r*cos(2.0*pi*rate)+cx;
+        y=r*sin(2.0*pi*rate)+cy;
+        glVertex2d(x+r,y-r);
+    }
+    glVertex2d(cx, cy);
+    for(i=-n-5;i<=0;i++){
+        rate = (double)i/n;
+        x=r*cos(2.0*pi*rate)+cx;
+        y=r*sin(2.0*pi*rate)+cy;
+        glVertex2d(x,y+r-0.001);
+    }
+    for(i=0;i<=n;i++){
+        rate = (double)i/n;
+        x=r*cos(2.0*pi*rate)+cx;
+        y=r*sin(2.0*pi*rate)+cy;
+        glVertex2d(x-r,y-r);
+    }
+    glEnd();
+}
+void draw_spade(float cx,float cy){
+    int i,n=100;
+    double pi=3.14159;
+    double rate;
+    float x,y,r=0.05900/2.0;
+    glPushMatrix();
+    glMatrixMode(GL_MODELVIEW);
+    glLoadIdentity();
+    glTranslatef(cx,cy,0.0);
+    glRotatef(180.0, 0.0, 0.0, 1.0);
+    glTranslatef(-cx,-cy,0.0);
+    glColor3d(0.0, 0.0, 0.0);
+    glBegin(GL_POLYGON);
+    glVertex2d(cx,  cy-0.09);
+    glVertex2d(cx+1.9*r,  cy-0.01);
+    for(i=0;i<n;i++){
+          rate = (double)i/n;
+          x=r*cos(2.0*pi*rate)+cx;
+          y=r*sin(2.0*pi*rate)+cy;
+          glVertex2d(x+r,y);
+    }
+    glVertex2d(cx, cy-r/2);
+    glVertex2d(cx+0.01, cy+0.07);
+    glVertex2d(cx-0.01, cy+0.07);
+    glVertex2d(cx, cy-r/2);
+    for(i=0;i<n;i++){
+        rate = (double)i/n;
+        x=r*cos(2.0*pi*rate)+cx;
+        y=r*sin(2.0*pi*rate)+cy;
+        glVertex2d(x-r,y);
+    }
+    glEnd();
+    glPopMatrix();
+}
+void draw_card(float x,float y,cards *card,int count){
+    int i;
+    float red;
+    for(i=0;i<count;i++){
+        if(card==dealer&&i==0&&mode==start){
+            glColor3d(0.6,0.2,0.2);
+            glBegin(GL_QUADS);
+            glVertex2d(x-0.1,  y-0.2);
+            glVertex2d(x+0.1,  y-0.2);
+            glVertex2d(x+0.1,  y+0.2);
+            glVertex2d(x-0.1,  y+0.2);
+            glEnd();
+            x += 0.21;
+            continue;
+        }
+        glColor3d(1.0, 1.0, 1.0);
+        glBegin(GL_QUADS);
+        glVertex2d(x-0.1,  y-0.2);
+        glVertex2d(x+0.1,  y-0.2);
+        glVertex2d(x+0.1,  y+0.2);
+        glVertex2d(x-0.1,  y+0.2);
+        glEnd();
+        switch (card[i].mark) {
+            case heart:
+                draw_heart(x,y);
+                red = 1.0;
+                break;
+            case diamond:
+                draw_diamond(x,y);
+                red = 1.0;
+                break;
+            case club:
+                draw_club(x,y);
+                red = 0.0;
+                break;
+            case spade:
+                draw_spade(x,y);
+                red = 0.0;
+                break;
+            default:
+                break;
+        }
+        glColor3d(red, 0.0, 0.0);
+        output(x-0.09,y+0.14,card[i].num);
+        output(x+0.05,y-0.18,card[i].num);
+        x += 0.21;
+    }
 }
 
+void disp(void)
+ {
+     char c1[4];
+     char c2[4];
+     glClear(GL_COLOR_BUFFER_BIT);
+     glColor3d(0.0, 0.0, 0.0);
+     output(-0.98,-0.95,"press q for quit");
+     if(mode==rule){
+         output(-0.98,-0.85,"press p for playing game");
+         float x=-0.5,y=0.8;
+         glColor3d(0.0, 0.0, 0.0);
+         output(x,y,"J,Q,K = 10points  2~10 = 2~10points  A = 1or11points");
+         output(x,y-0.2,"-> Dealer and Player start with two cards.");
+         output(x,y-0.4,"-> HIT = Draw cards.");
+         output(x,y-0.6,"-> but when your hands totales exceed 21, you lose (BURST).");
+         output(x,y-0.8,"-> STAND = Compare your total with dealer's total.");
+         output(x,y-1.0,"-> The person with the most totale wins!");
+         output(x,y-1.2,"-> Dealer draws cards until dealer's total exceed 17.");
+     }
+     else if(mode==betting){
+         sprintf(c1, "%d", tip);
+         glColor3d(0.0, 0.0, 0.0);
+         output(-0.98,-0.75,"press space to show rule");
+         output(-0.1,0.5,"your tips is ");
+         output(0.0,0.4,c1);
+         output(-0.25,0.3,"How many tips do you bet?");
+         output(0.0,0.2,s);
+         if(errar==1){
+             output(-0.25,0.1,"press number 1~ and enter");
+         }
+         else{
+             errar=1;
+             tip-=bet;
+             mode=start;
+             glutPostRedisplay();
+         }
+     }
+     else{
+         draw_card(-0.6,0.5,dealer,dcount);
+         draw_card(-0.6,-0.5,player,pcount);
+         glColor3d(0.0, 0.0, 0.0);
+         output(-0.98,-0.85,"press space to show rule");
+         output(0.8,0.8,"your tips");
+         sprintf(c1, "%d", tip);
+         output(0.85,0.7,c1);
+         output(0.85,0.6,"bet");
+         sprintf(c2, "%d", bet);
+         output(0.85,0.5,c2);
+         if(mode==start){
+               output(-0.15,0.1,"press h for HIT");
+               output(0.0,0.0,"or");
+               output(-0.15,-0.1,"press s for STAND");
+               if(p_judge==2){
+                   result1=burst;
+                   if(tip<=0){
+                       mode=gameover;
+                   }
+                   else{
+                       mode=end;
+                   }
+               }
+               else if(p_judge==1){
+                   d_judge = judge_dealer();
+                   while(d_judge==0){
+                       get_card(dealer,dcount);
+                       dcount++;
+                       d_judge = judge_dealer();
+                       glutPostRedisplay();
+                   }
+                   result1=judge_game();
+               }
+             glutPostRedisplay();
+         }
+         else if(mode==end||mode==gameover){
+             if(mode==gameover){
+                 glColor3d(1.0, 0.0, 0.0);
+                 output(-0.05,-0.15,"GAME OVER");
+                 output(-0.06,-0.25,"press r for resset");
+             }
+             else{
+                 output(-0.1,-0.15,"press c for continue");
+             }
+             switch (result1) {
+                 case win:
+                     output(0.0,0.0,"you win!");
+                     break;
+                 case lose:
+                     output(0.0,0.0,"you lose!");
+                     break;
+                 case draw:
+                     output(0.0,0.0,"draw!");
+                     break;
+                 case brackjack:
+                     output(0.0,0.0,"brackjack!");
+                     break;
+                 case just:
+                     output(0.0,0.0,"you win just 21!");
+                     break;
+                 case burst:
+                     output(0.0,0.0,"BURST!");
+                     break;
+                 default:
+                     break;
+             }
+             glutPostRedisplay();
+         }
+     }
+     glFlush();
+     glutSwapBuffers();
+ }
+ void init(void)
+ {
+     if(mode==betting||mode==gameover){
+      shuffle();
+      deal_card();
+      p_judge=judge_player();
+     }
+         float red = 100.0f/255.0f;  // 赤色
+         float green = 155.0f/255.0f;    // 緑色
+         float blue = 0.0f/255.0f;   // 青色
+         float alpha = 0.0f/100.0f;  // 透明度
+         glClearColor(red, green, blue, alpha);
+ }
+void keyboard(unsigned char key, int x, int y) {
+    if(mode==betting){
+        static int i=0;
+        if(key>='0'&&key<='9'){
+            s[i]=key;
+            i++;
+        }
+        else if(key=='\015'){
+            bet=atoi(s);
+            if(s[0]=='\0'||bet>tip||bet<=0){
+                errar=1;
+            }
+            else{
+                errar=0;
+            }
+            i=0;
+            memset( s, '\0', sizeof(s) );
+        }
+        glutPostRedisplay();
+    }
+    switch (key) {
+        case 'q':
+        case 'Q':
+        case '\033':  // esc
+          exit(0);
+          break;
+        case 'h':
+          if(mode==start){
+             get_card(player,pcount);
+             pcount++;
+             p_judge = judge_player();
+             glutPostRedisplay();
+          }
+          break;
+        case 's':
+          if(mode==start){
+             p_judge=1;
+             glutPostRedisplay();
+          }
+          break;
+        case 'r':
+          if(mode==gameover){
+            init();
+            glutPostRedisplay();
+          }
+          break;
+        case ' ':
+          pmode=mode;
+          mode=rule;
+          init();
+          glutPostRedisplay();
+          break;
+        case 'p':
+          if(mode==rule){
+              mode=pmode;
+              init();
+              glutPostRedisplay();
+          }
+         break;
+        case 'c':
+          if(mode==end){
+            mode=betting;
+            init();
+            glutPostRedisplay();
+          }
+          break;
+        default:
+          break;
+        }
+  
+  
+}
 
-/*void display(){
-	//pass
-}*/
+int main(int argc, char *argv[])
+{
+    
+     glutInit(&argc, argv);
+     glutInitDisplayMode(GLUT_DEPTH|GLUT_DOUBLE|GLUT_RGBA );
+     glutInitWindowPosition( 100, 100 );
+     glutInitWindowSize( 1000, 700 );
+     glutCreateWindow("Brack_Jack");
+     init();
+     glutDisplayFunc(disp);
+     glutKeyboardFunc(keyboard);
+     glutMainLoop();
+     return 0;
+}
